e2e393a5543559f7af06ceb20e94d9344d4bfde6
sergeklokov <serge.klokov@gmail.com>
2015-07-07 13:15:26 -0700

    init
    MD5 and SHA256 salted hashing

---
+++ MD5HashDemo/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Configuration; // add .dll in Reference please
+using System.Collections.Specialized;
+
+namespace MD5HashDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = "Hello World!";
+
+            // version I
+            using (MD5 md5Hash = MD5.Create())
+            {
+                //var hashCode = md5Hash.GetHashCode();
+                //var x = md5Hash.Initialize();
+
+                string hash = GetMd5Hash(md5Hash, source); 
+
+                Console.WriteLine("The MD5 hash of " + source + " is: " + hash + ".");
+
+                Console.WriteLine("Verifying the hash...");
+
+                if (VerifyMd5Hash(md5Hash, source, hash))
+                {
+                    Console.WriteLine("The hashes are the same.");
+                }
+                else
+                {
+                    Console.WriteLine("The hashes are not same.");
+                }
+            }
+
+            Console.WriteLine();
+
+            // version II with getting/storing hash key in app.conig
+            string hashSaltString = ConfigurationManager.AppSettings.Get("hashSalt");
+            byte[] hashSaltBytes = System.Convert.FromBase64String(hashSaltString);  // convert to bytes
+
+            var id = 3; // we will hash it
+            string strId = id.ToString();
+
+            var hashed = ComputeSHA256Hash(strId, hashSaltBytes);
+            Console.WriteLine("Algorithm II with salt");
+            Console.WriteLine("hashed {0} = {1}", id, hashed);
+            Console.WriteLine(VerifySHA256Hash(strId, hashed));
+            Console.WriteLine();
+
+            var hashedMD5 = ComputeMD5Hash(strId, hashSaltString);
+            Console.WriteLine("Algorithm MD5 with salt");
+            Console.WriteLine("MD5 hashed {0} = {1}", id, hashedMD5);
+            Console.WriteLine(CheckMD5Hash(strId, hashedMD5));
+
+            Console.ReadKey();
+
+        }
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash. 
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes 
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string. 
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string. 
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input. 
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // overload
+        public static string ComputeSHA256Hash(string plainText, string salt)
+        { 
+            byte[] saltBytes = System.Convert.FromBase64String(salt);
+
+            string hash = ComputeSHA256Hash(plainText, saltBytes);
+
+            return hash;
+        }
+
+        public static string ComputeSHA256Hash(string plainText, byte[] saltBytes)
+        {
+            // Convert plain text into a byte array.
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+        
+            // Allocate array, which will hold plain text and salt.
+            byte[] plainTextWithSaltBytes = new byte[plainTextBytes.Length + saltBytes.Length];
+
+            // Copy plain text bytes into resulting array.
+            for (int i=0; i < plainTextBytes.Length; i++)
+                plainTextWithSaltBytes[i] = plainTextBytes[i];
+        
+            // Append salt bytes to the resulting array.
+            for (int i=0; i < saltBytes.Length; i++)
+                plainTextWithSaltBytes[plainTextBytes.Length + i] = saltBytes[i];
+
+            HashAlgorithm hash = new SHA256Managed();
+      
+            // Compute hash value of our plain text with appended salt.
+            byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+        
+            // Create array which will hold hash and original salt bytes.
+            byte[] hashWithSaltBytes = new byte[hashBytes.Length + saltBytes.Length];
+        
+            // Copy hash bytes into resulting array.
+            for (int i=0; i < hashBytes.Length; i++)
+                hashWithSaltBytes[i] = hashBytes[i];
+            
+            // Append salt bytes to the result.
+            for (int i=0; i < saltBytes.Length; i++)
+                hashWithSaltBytes[hashBytes.Length + i] = saltBytes[i];
+            
+            // Convert result into a base64-encoded string.
+            string hashValue = Convert.ToBase64String(hashWithSaltBytes);
+        
+            // Return the result.
+            return hashValue;
+        }
+
+        public static bool VerifySHA256Hash(string plainText, string hashValue)
+        {
+            // Convert base64-encoded hash value into a byte array.
+            byte[] hashWithSaltBytes = Convert.FromBase64String(hashValue);
+        
+            int hashSizeInBits = 256;
+
+            // Convert size of hash from bits to bytes.
+            int hashSizeInBytes = hashSizeInBits / 8;
+
+            // Make sure that the specified hash value is long enough.
+            if (hashWithSaltBytes.Length < hashSizeInBytes)
+                return false;
+
+            // Allocate array to hold original salt bytes retrieved from hash.
+            byte[] saltBytes = new byte[hashWithSaltBytes.Length - hashSizeInBytes];
+
+            // Copy salt from the end of the hash to the new array.
+            for (int i=0; i < saltBytes.Length; i++)
+                saltBytes[i] = hashWithSaltBytes[hashSizeInBytes + i];
+
+            // Compute a new hash string.
+            string expectedHashString = ComputeSHA256Hash(plainText, saltBytes);
+
+            // If the computed hash matches the specified hash,
+            // the plain text value must be correct.
+            return (hashValue == expectedHashString);
+        }
+
+        // ************************* MD5 *************************
+        public static string ComputeMD5Hash(string plainText, string salt)
+        {
+            byte[] saltBytes = System.Convert.FromBase64String(salt);
+
+            string hash = ComputeMD5Hash(plainText, saltBytes);
+
+            return hash;
+        }
+
+        public static string ComputeMD5Hash(string plainText, byte[] saltBytes)
+        {
+            // Convert plain text into a byte array.
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            // Allocate array, which will hold plain text and salt.
+            byte[] plainTextWithSaltBytes = new byte[plainTextBytes.Length + saltBytes.Length];
+
+            // Copy plain text bytes into resulting array.
+            for (int i = 0; i < plainTextBytes.Length; i++)
+                plainTextWithSaltBytes[i] = plainTextBytes[i];
+
+            // Append salt bytes to the resulting array.
+            for (int i = 0; i < saltBytes.Length; i++)
+                plainTextWithSaltBytes[plainTextBytes.Length + i] = saltBytes[i];
+
+            HashAlgorithm hash = new MD5CryptoServiceProvider();
+
+            // Compute hash value of our plain text with appended salt.
+            byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+
+            // Create array which will hold hash and original salt bytes.
+            byte[] hashWithSaltBytes = new byte[hashBytes.Length + saltBytes.Length];
+
+            // Copy hash bytes into resulting array.
+            for (int i = 0; i < hashBytes.Length; i++)
+                hashWithSaltBytes[i] = hashBytes[i];
+
+            // Append salt bytes to the result.
+            for (int i = 0; i < saltBytes.Length; i++)
+                hashWithSaltBytes[hashBytes.Length + i] = saltBytes[i];
+
+            // Convert result into a base64-encoded string.
+            string hashValue = Convert.ToBase64String(hashWithSaltBytes);
+
+            // Return the result.
+            return hashValue;
+        }
+
+        public static bool CheckMD5Hash(string plainText, string hashValue)
+        {
+            // Convert base64-encoded hash value into a byte array.
+            byte[] hashWithSaltBytes = Convert.FromBase64String(hashValue);
+
+            int hashSizeInBits = 128; // for MD5
+
+            // Convert size of hash from bits to bytes.
+            int hashSizeInBytes = hashSizeInBits / 8;
+
+            // Make sure that the specified hash value is long enough.
+            if (hashWithSaltBytes.Length < hashSizeInBytes)
+                return false;
+
+            // Allocate array to hold original salt bytes retrieved from hash.
+            byte[] saltBytes = new byte[hashWithSaltBytes.Length - hashSizeInBytes];
+
+            // Copy salt from the end of the hash to the new array.
+            for (int i = 0; i < saltBytes.Length; i++)
+                saltBytes[i] = hashWithSaltBytes[hashSizeInBytes + i];
+
+            // Compute a new hash string.
+            string expectedHashString = ComputeMD5Hash(plainText, saltBytes);
+
+            // If the computed hash matches the specified hash,
+            // the plain text value must be correct.
+            return (hashValue == expectedHashString);
+        }
+    }
+    
+}
